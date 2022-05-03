a2d36cfd3e7abc754e41307877d9698119f6821e
kaylendog <actuallyori@gmail.com>
2022-05-03 10:56:21 +0100

    chore: fixup settings

--- src/document/services/block.service.ts
+++ src/document/services/block.service.ts
@@ -3,7 +3,7 @@ import { Model } from "mongoose";
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 
-import { RootBlockSchema } from "../schemas/block.schema";
+import { IRootBlock, RootBlockSchema } from "../schemas/block.schema";
 
 @Injectable()
 export class BlockService {
@@ -11,4 +11,8 @@ export class BlockService {
 		@InjectModel(RootBlockSchema.name)
 		private readonly rootBlockModel: Model<IRootBlock>
 	) {}
+
+	async getRootBlock(id: string): Promise<IRootBlock> {
+		return this.rootBlockModel.findOne({ id });
+	}
 }
--- src/document/services/document.service.ts
+++ src/document/services/document.service.ts
@@ -1,23 +1,40 @@
 import { Model } from "mongoose";
 
+import {
+	AnyBlock,
+	BlockType,
+	Document,
+	DocumentRevision,
+} from "@dedit/models/src/v1";
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 
-import { DocumentSchema, IDocument } from "../schemas/document.schema";
-
-import type { Document } from "@dedit/models/src/v1";
+import { IRootBlock, RootBlockSchema } from "../schemas/block.schema";
+import {
+	DocumentSchema,
+	DocumentWithIds,
+	IDocument,
+} from "../schemas/document.schema";
+import {
+	DocumentRevisionSchema,
+	IDocumentRevision,
+} from "../schemas/revision.schema";
 
 @Injectable()
 export class DocumentService {
 	constructor(
 		@InjectModel(DocumentSchema.name)
-		private readonly documentModel: Model<IDocument>
+		private readonly documentModel: Model<IDocument>,
+		@InjectModel(RootBlockSchema.name)
+		private readonly rootBlockSchema: Model<IRootBlock>,
+		@InjectModel(DocumentRevisionSchema.name)
+		private readonly revisionModel: Model<IDocumentRevision>
 	) {}
 
-	async findManyRaw(): Promise<Document[]> {
+	async findManyRaw(): Promise<DocumentWithIds[]> {
 		return this.documentModel.find().exec();
 	}
-	findOneRaw(id: string): Promise<Document> {
+	findOneRaw(id: string): Promise<DocumentWithIds> {
 		return this.documentModel.findOne({ id }).exec();
 	}
 
@@ -25,10 +42,44 @@ export class DocumentService {
 	 * Recursively looks up a document.
 	 * @param id
 	 */
-	async recursiveFind(id: string): Promise<Document> {
-		const root = (await this.documentModel.findOne({ id })) as Document;
+	async constructDocument(id: string): Promise<Document> {
+		const root = await this.documentModel.findOne({ id });
 		if (!root) {
 			return;
 		}
+		// look up revisions
+		const revisions = (await this.revisionModel.find({
+			id: { $in: root.revisions },
+		})) as DocumentRevision[];
+		const resolvedRevisions = await Promise.all(
+			revisions.map((revision) => this.constructBlock(revision))
+		);
+		return {
+			...root,
+			revisions: resolvedRevisions,
+		};
+	}
+
+	/*
+	 * Recursively constructs a block tree.
+	 * @param block The root block.
+	 * @returns
+	 */
+	async constructBlock<T extends AnyBlock | DocumentRevision>(
+		block: T
+	): Promise<T> {
+		// look up references and replace
+		if (block.type === BlockType.Ref) {
+			block = await this.rootBlockSchema.findOne({
+				id: block.data.id,
+			});
+		}
+		// recursively construct children of blocks
+		if ("children" in block) {
+			block.children = await Promise.all(
+				block.children.map((child: AnyBlock) => this.constructBlock(child))
+			);
+		}
+		return block;
 	}
 }
